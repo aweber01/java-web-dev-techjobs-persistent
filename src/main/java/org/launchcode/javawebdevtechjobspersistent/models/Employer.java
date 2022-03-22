110289193c7cacd45cddf8b398ba9f7e9558648c
Arielle <ariellecweber@gmail.com>
2022-03-21 20:22:32 -0500

    Part 3 first attempt

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,7 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -9,6 +13,10 @@ public class Employer extends AbstractEntity {
     @NotBlank
     @Size(max = 100, message = "Exceeds maximum of 100 characters.")
     private String location;
+
+    @OneToMany(mappedBy = "employer")
+    //@JoinColumn(name = "id")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer(String location) {
         super();
