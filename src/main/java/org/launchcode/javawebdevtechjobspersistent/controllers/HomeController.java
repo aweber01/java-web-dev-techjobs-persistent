bb98e48ab189d0cc3f62bc47a9778bd9dc899d3b
Arielle <ariellecweber@gmail.com>
2022-03-23 18:06:16 -0500

    Completed

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -71,8 +71,15 @@ public class HomeController {
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
+        Optional<Job> optJob = jobRepository.findById(jobId);
 
-        return "view";
+        if (optJob.isPresent()) {
+            Job job = (Job) optJob.get();
+            model.addAttribute("job", job);
+            return "view";
+        } else {
+            return "redirect:../";
+        }
     }
 
 
